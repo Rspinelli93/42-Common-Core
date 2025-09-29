612b775a885b81d6e6ba6b6b2b80b17e07ebc3cc
Riccardo Spinelli <rick@MacBook-Pro-de-Riccardo.local>
2025-09-29 11:45:51 +0200

    small fix return

--- srcs/ft_isalpha.c
+++ srcs/ft_isalpha.c
@@ -15,8 +15,8 @@
 int ft_isalpha(int c)
 {
     if (c >= 65 && c <= 90)
-        return (1024);
+        return (1);
     if (c >= 97 && c <= 122)
-        return (1024);
+        return (1);
     return (0);
 }
