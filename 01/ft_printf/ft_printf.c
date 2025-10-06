25de03585c902a641c72a0b87373273ed29f1458
Riccardo Spinelli Ranero <rspinell@c1r13s7.42lausanne.ch>
2025-10-06 16:13:09 +0200

    rm.o

---
+++ 01/ft_printf/ft_printf.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rspinell <rspinell@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/06 15:13:43 by rspinell          #+#    #+#             */
+/*   Updated: 2025/10/06 16:09:17 by rspinell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_printf(const char *format, ...)
+{
+
+}
+/*
+• %c Prints a single character.
+• %s Prints a string (as defined by the common C convention).
+• %p The void * pointer argument has to be printed in hexadecimal format.
+• %d Prints a decimal (base 10) number.
+• %i Prints an integer in base 10.
+• %u Prints an unsigned decimal (base 10) number.
+• %x Prints a number in hexadecimal (base 16) lowercase format.
+• %X Prints a number in hexadecimal (base 16) uppercase format.
+• %% Prints a percent sign.
+*/
