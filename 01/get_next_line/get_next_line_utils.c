9c09b7a08d9bbb203bcfc0a04f12b93b00dffb7e
Riccardo Spinelli <rspinellir13@gmail.com>
2025-10-12 18:42:57 +0200

    N

--- 01/get_next_line/get_next_line_utils.c
+++ 01/get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: rick <rick@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/12 12:34:25 by rspinell          #+#    #+#             */
-/*   Updated: 2025/10/12 18:00:29 by rick             ###   ########.fr       */
+/*   Updated: 2025/10/12 18:39:01 by rick             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,23 @@ int	found_n(char *str)
 	}
 	return (0);
 }
-void	concat_end(static char *stat, char *str)
+/*
+* concat_end concatenates at the end of the staric string 
+* (string that will be returned by get_next_line), the part
+* of the buffer before finding '\n' or '\0'.
+* Return: the return is null in case of coping the full buffer,
+* and in case of stopping before by a '\n' it will return a pointer
+* to the next char. */
+char	*concat_end(static char *stat, char *buff)
 {
-	
+	stat += ft_strlen(stat);
+	while (*buff || *buff == '\n')
+	{
+		*stat = *buff;
+		buff++;
+		stat++;
+	}
+	if (*buff == '\n')
+		return (buff + 1);
+	return (NULL);
 }
