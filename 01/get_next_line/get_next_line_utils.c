1623b623fe75b177e14ba4649372b816b9c3399a
Riccardo Spinelli <rspinellir13@gmail.com>
2025-10-12 18:01:16 +0200

    N

--- 01/get_next_line/get_next_line_utils.c
+++ 01/get_next_line/get_next_line_utils.c
@@ -3,10 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rspinell <rspinell@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rick <rick@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/12 12:34:25 by rspinell          #+#    #+#             */
-/*   Updated: 2025/10/12 12:40:12 by rspinell         ###   ########.fr       */
+/*   Updated: 2025/10/12 18:00:29 by rick             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+/*
+* Boolean function to check if the string 
+* contains a new line char ('\n'). */
+int	found_n(char *str)
+{
+	while (*str)
+	{
+		if (*str == '\n')
+			return (1);
+		str++;
+	}
+	return (0);
+}
+void	concat_end(static char *stat, char *str)
+{
+	
+}
