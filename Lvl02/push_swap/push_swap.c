dc32c1f206651768f667998b9a1a60502c405d68
Riccardo Spinelli <rspinellir13@gmail.com>
2025-11-03 20:56:37 +0100

    Norm

--- Lvl02/push_swap/push_swap.c
+++ Lvl02/push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: rick <rick@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 18:35:41 by rspinell          #+#    #+#             */
-/*   Updated: 2025/11/03 19:22:32 by rick             ###   ########.fr       */
+/*   Updated: 2025/11/03 20:56:26 by rick             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,17 @@
 int	main(int argc, char **argv)
 {
 	return (0);
+	// checker function:
+	// - check if all numbers
+	// - check amount of args
+	// - print error in case
+
+	// Create linked list function:
+	// - Iterate through the array of num
+	// - Create for each a new struct
+	// - During creation, make the connections
+	// - Do null terminate the last one
+
+	// Create algorithm comparing both
+	// - Duplication at beggining?
 }
