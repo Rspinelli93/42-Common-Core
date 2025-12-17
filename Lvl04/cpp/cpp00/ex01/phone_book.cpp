b59d1943f9bc73a260cdf1cee746bd38bddf6bc8
Rspinell93 <rspinellir13@gmail.com>
2025-12-17 13:02:32 +0100

    cpp

--- Lvl04/cpp/cpp00/ex01/phone_book.cpp
+++ Lvl04/cpp/cpp00/ex01/phone_book.cpp
@@ -6,7 +6,7 @@
 /*   By: rick <rick@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 16:42:11 by rick              #+#    #+#             */
-/*   Updated: 2025/12/13 17:13:01 by rick             ###   ########.fr       */
+/*   Updated: 2025/12/17 13:01:26 by rick             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,73 @@
 
 int main(void)
 {
-    char *command;
-
+    std::string command;
+    PhoneBook   my_phonebook;
+    std::string prompt;
+    Contact     temp;
     while (1)
     {
         std::cin >> command;
         if (command == "EXIT")
             break ;
         if (command == "SEARCH")
-            break ;
+        {
+            int i = 0;
+            while (i < 8)
+            {
+                std::cout << my_phonebook.book[i].first_name;
+                i++;
+            }
+        }
         if (command == "ADD")
-            break ;
+        {
+            std::cout << "Enter contact First name: ";
+            std::cin >> prompt;
+            while (prompt == "")
+            {
+                std::cout << "Enter contact First name: ";
+                std::cin >> prompt;
+            }
+            my_phonebook.set_name(prompt, temp);
+            std::cout << "\n";
+            std::cout << "Enter contact Last name: ";
+            std::cin >> prompt;
+            while (prompt == "")
+            {
+                std::cout << "Enter contact Last name: ";
+                std::cin >> prompt;
+            }
+            my_phonebook.set_surname(prompt, temp);
+            std::cout << "\n";
+            std::cout << "Enter contact Nickname: ";
+            std::cin >> prompt;
+            while (prompt == "")
+            {
+                std::cout << "Enter contact Nickname: ";
+                std::cin >> prompt;
+            }
+            my_phonebook.set_nickname(prompt, temp);
+            std::cout << "\n";
+            std::cout << "Enter contact Phone number: ";
+            std::cin >> prompt;
+            while (prompt == "")
+            {
+                std::cout << "Enter contact Phone number: ";
+                std::cin >> prompt;
+            }
+            my_phonebook.set_number(prompt, temp);
+            std::cout << "\n";
+            std::cout << "Enter contact Darkest Secret: ";
+            std::cin >> prompt;
+            while (prompt == "")
+            {
+                std::cout << "Enter contact Darkest Secret: ";
+                std::cin >> prompt;
+            }
+            my_phonebook.set_secret(prompt, temp);
+            std::cout << "\n";
+            my_phonebook.next_contact();
+        }
     }
     return (0);
 }
--- Lvl04/cpp/cpp00/ex01/phone_book.hpp
+++ Lvl04/cpp/cpp00/ex01/phone_book.hpp
@@ -6,7 +6,7 @@
 /*   By: rick <rick@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/13 16:36:54 by rick              #+#    #+#             */
-/*   Updated: 2025/12/13 17:14:22 by rick             ###   ########.fr       */
+/*   Updated: 2025/12/17 13:00:09 by rick             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,25 @@
 
 class Contact {
     public:
-        char *name;
-        void set_name(char *input)
-        {
-            name = input;
-        }
+        std::string first_name;
+        std::string last_name;
+        std::string nickname;
+        std::string phone_number;
+        std::string secret;
 };
 
 class PhoneBook {
     public:
-        Contact *book;
+        Contact book[8];
         int i;
+        void set_name(std::string name, Contact temp) { temp.first_name = name; }
+        void set_surname(std::string name, Contact temp) { temp.last_name = name; }
+        void set_nickname(std::string name, Contact temp) { temp.nickname = name; }
+        void set_number(std::string number, Contact temp) { temp.phone_number = number; }
+        void set_secret(std::string secret, Contact temp) { temp.secret = secret; }
+        void add_contact(Contact temp) { book[i] = temp; }
+        void next_contact() { if(i == 7) i = 0; else i++; }
+        PhoneBook(){ i = 0; }
 };
 
 #endif
