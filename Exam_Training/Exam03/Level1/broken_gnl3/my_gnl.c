97f6bcd461d6d073f9bc16d38853ff8c627384f6
Riccardo Spinelli Ranero <rspinell@c1r13s2.42lausanne.ch>
2026-02-03 17:39:24 +0100

    training

--- Exam_Training/Exam03/Level1/broken_gnl3/my_gnl.c
+++ Exam_Training/Exam03/Level1/broken_gnl3/my_gnl.c
@@ -1,122 +1,50 @@
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 5
+#endif
+
 #include <stdio.h>
-#include <unistd.h>
 #include <fcntl.h>
-#include <stdlib.h>
-#define BUFFER_SIZE 5
+#include <unistd.h>
 
 int ft_strlen(char *str)
 {
-    int i = 0;
 
-    if (!str)
-        return (0);
-
-    while (str[i])
-        i++;
-    return (i);
 }
 
-char    *ft_strchr(char *str, char c)
+char *ft_strchr(char *str, char c)
 {
-    if (!str)
-        return (NULL);
-    while (*str)
-    {
-        if (*str == c)
-            return (str);
-        str++;
-    }
-    if (*str == c)
-        return (str);
-    return (NULL);
+
 }
 
-char    *ft_strjoin(char *line, char *buff)
+char *ft_strjoin(char *str, char *buff)
 {
-    char *new;
-    int len = ft_strlen(line) + ft_strlen(buff) + 1;
-    int i = 0;
-    int j = 0;
 
-    if (!line && !*buff)
-		return (NULL);
-    new = malloc(sizeof(char) * len);
-    if (!new)
-    {
-        if (line)
-            free(line);
-        return (NULL);
-    }
-    while (line && *line)
-    {
-        new[i] = line[j];
-        i++;
-        j++;
-    }
-    j = 0;
-    while (buff && buff[i])
-    {
-        new[i] = buff[j];
-        i++;
-        j++;
-    }
-    new[i] = '\0';
-    if (line)
-        free(line);
-    return (new);
 }
 
-static void save_buff(char *line, char *buff)
+void save_buff(char *line, char *buff)
 {
-    char *ptr;
-    int i = 0;
-    ptr = ft_strchr(line, '\n');
-    if (!ptr)
-        return ;
-    ptr++;
-    while (*ptr)
-    {
-        buff[i] = *ptr;
-        *ptr = '\0';
-        ptr++;
-        i++;
-    }
-    buff[i] = '\0';
+
 }
 
 char *get_next_line(int fd)
 {
-    int ret;
-    char *line = NULL;
-    static char buff[BUFFER_SIZE + 1];
+	char buff[BUFFER_SIZE + 1];
+	char *line = NULL;
+	int ret = 0;
 
-    if (fd < 0 || BUFFER_SIZE < 1)
-        return (NULL);
-    line = ft_strjoin(line, buff);
-    while (!ft_strchr(line, '\n'))
-    {
-        ret = read(fd, buff, BUFFER_SIZE);
-        if (ret < 1)
-            break ;
-        buff[ret] = 0;
-        line = ft_strjoin(line, buff);
-        buff[0] = '\0';
-    }
-    buff[0] = '\0';
-    save_buff(line, buff);
-    return (line);
-}
-
-int main(void)
-{
-    char *str;
-    int fd = open("/home/rick/Documents/Common-Core/Lvl01/gnl/tests/files/test_alternate_multiple_new_line" ,O_RDONLY);
-    while(str = get_next_line(fd))
-    {
-        printf("%s", str);
-        free(str);
-    }
-    free(str);
-    close(fd);
-    return (0);
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
+	line = ft_strjoin(line, buff);
+	while (!ft_strchr(line, '\n'))
+	{
+		ret = read(fd, buff, BUFFER_SIZE);
+		if (ret < 1)
+			break;
+		buff[ret] = '\0';
+		ft_strjoin(line, buff);
+		buff[0] = '\0';
+	}
+	buff[0] = '\0';
+	save_buff(line, buff);
+	return (line);
 }
--- Exam_Training/Exam03/Level1/filter/filter.c
+++ Exam_Training/Exam03/Level1/filter/filter.c
@@ -1,144 +1,124 @@
-#include <unistd.h>
+// Assignment name: filter
+// Expected files: filter.c
+// Allowed functions: read, write, strlen, memmem, memmove, malloc, calloc,
+// realloc, free, printf, fprintf, stdout, stderr, perror
+// --------------------------------------------------------------------------------
+
+// Write a program that will take one and only one argument.
+
+// Your program will then read from stdin and write all the content read in stdout
+// except that every occurrence of s must be replaced by '*' (as many as the length
+// of s). Your program will be tested with random buffer sizes, using a custom read
+// function. Therefore the buffer being set in your program will be filled with a
+// different number of chars each new call.
+
+
+// For example:
+
+// ./filter bonjour
+// will behave in the same way as:
+// sed 's/bonjour/*******/g'
+
+// ./filter abc
+// will behave in the same way as:
+// sed's/abc/***/g'
+
+// More generally your program must be the equivalent of the shell script filter.sh
+// present in this directory (you can compare your program with it).
+
+// In case of error during a read or a malloc, you must write "Error: " followed by
+// the error message in stderr and return 1.
+
+// If the program is called without arguments or with an empty argument or with multiple
+// arguments, it must return 1.
+
+// For example this should work:
+
+// $> echo 'abcdefaaaabcdeabcabcdabc' | ./filter abc | cat -e
+// ***defaaa***de******d***$
+// $> echo 'ababcabababc' | ./filter ababc | cat -e
+// *****ab*****$
+// $>
+
+// NOTES:
+// memmem includes:
+//                 #define _GNU_SOURCE
+// 			    #include <string.h>
+
+// perror includes:
+//                 #include <errno.h>
+
+// read includes:
+//                 #include <fcntl.h>
+
 #include <string.h>
-#include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
 #include <errno.h>
 
-#define BUFFER_SIZE 2
+int	ft_strcmp(char *str, char *occ, int size)
+{
+	int i = 0;
 
-int     ft_strlen(char *str);
-char    *str_realloc(char *str, char *buffer);
-void print_string(char *str, char *av);
+	while (occ[i] == str[i] && str[i] && occ[i] && i < size)
+		i++;
+	if (occ[i] == '\0')
+		return (1);
+	return (0);
+}
+
+void set_asterix(char *str, int size)
+{
+	int i = 0;
+
+	while (size > i && str[i])
+	{
+		str[i] = '*';
+		i++;
+	}
+}
+
+char *ft_filter(char *string, char *occ)
+{
+	int size = strlen(occ);
+	char *str = string;
+	if (!string)
+		return (NULL);
+	while(str && *str)
+	{
+		if (ft_strcmp(str, occ, size))
+		{
+			set_asterix(str, size);
+			str += size;
+		}
+		if (*str == '\0')
+			break;
+		str++;
+	}
+	return (string);
+}
 
 int main(int ac, char **av)
 {
-    char    *buff;
-    char    *str;
-    int     ret;
+	char buff[1000000];
+	int i = 0;
+	int ret;
 
-    // 1. Initialize str to empty "" so it's never NULL
-    str = calloc(1, 1);
-    if (!str)
-        return (1);
-
-    if (ac == 2)
-    {
-        // 2. Alloc buffer (Size + 1 for safety)
-        buff = calloc(sizeof(char), BUFFER_SIZE + 1);
-        if (!buff)
-            return (free(str), 1);
-
-        // 3. Read Loop
-        while ((ret = read(STDIN_FILENO, buff, BUFFER_SIZE)) > 0)
-        {
-            buff[ret] = '\0'; // Just cap the string at ret. No loop needed.
-            str = str_realloc(str, buff);
-            int k = 0;
-            while (buff[k])
-            {
-                if (buff[k] == '\n')
-                {
-                    // You might want to handle removing the \n if needed
-                    break; 
-                }
-                k++;
-            }
-            if (buff[k] == '\n')
-                break; // Break the main read loop
-        }
-        
-        // 4. Handle Errors or Print
-        if (ret == -1)
-        {
-            free(buff);
-            free(str);
-            return (perror("Error: "), -1);
-        }
-        print_string(str, av[1]); // str is safe now
-        printf("\n");
-        free(buff);
-        free(str);
-        return (0);
-    }
-    free(str); // Don't forget to free if ac != 2
-    return (1);
+	if(ac != 2 || av[1][0] == '\0')
+		return (1);
+	while ((ret = read(0, buff + i, 10)) > 0)
+	{
+		i = i + ret;
+		buff[i] = '\0';
+	}
+	printf("%s", ft_filter(buff, av[1]));
+	return (1);
 }
 
-char    *str_realloc(char *str, char *buffer)
-{
-    char    *new_str;
-    int     len_str;
-    int     len_buff;
-    int     i = 0;
-    int     j = 0;
-
-    // Calculate lengths to know exactly how much to alloc
-    len_str = ft_strlen(str);
-    len_buff = ft_strlen(buffer);
-
-    // Allocate new size (Old + New + 1 for \0)
-    new_str = calloc(sizeof(char), len_str + len_buff + 1);
-    if (!new_str)
-        return (free(str), NULL); // Handle alloc fail
-
-    // Copy part 1: The old string
-    while (i < len_str)
-    {
-        new_str[i] = str[i];
-        i++;
-    }
-
-    // Copy part 2: The new buffer
-    while (j < len_buff)
-    {
-        new_str[i] = buffer[j];
-        i++;
-        j++;
-    }
-    
-    // Null terminate
-    new_str[i] = '\0';
-    
-    // Free the old string and return new one
-    free(str);
-    return (new_str);
-}
-
-int ft_strlen(char *str)
-{
-    int i = 0;
-    while (str && str[i])
-        i++;
-    return (i);
-}
-
-void print_string(char *str, char *av)
-{
-    int i;
-    int j;
-    int k;
-
-    i = 0;
-    while (str[i])
-    {
-        j = 0;
-        k = i;
-        while (av[j])
-        {
-            if (av[j] == str[k])
-                k++;
-            j++;
-        }
-        if (j == k - i)
-        {
-            k = i;
-            while (j != k - i)
-            {
-                str[k] = '*';
-                k++;
-            }
-        }
-        i++;
-    }
-    printf("%s", str);
-}
+// int main(void)
+// {
+// 	printf("%s\n", ft_filter(ft_strdup("hola abcdefg"), "abc"));
+// 	return (0);
+// }
