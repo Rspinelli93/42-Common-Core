87d5835d597a3c3fdbc76183123ad9032bb38ecc
Rick <rspinellir13@gmail.com>
2026-02-03 12:10:33 +0100

    training

--- Exam_Training/Exam03/Level1/ft_printf_2/ft_printf.c
+++ Exam_Training/Exam03/Level1/ft_printf_2/ft_printf.c
@@ -35,8 +35,105 @@ Warning: Your function must not have any leaks, this will be checked during moul
 #include <unistd.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <limits.h>
 
-int ft_printf(const char *, ... )
+void    ft_putchar(char c, int *ret)
 {
+    write(1, &c, 1);
+    *ret += 1;
+}
+
+void    ft_putstr(char *str, int *ret)
+{
+    while (str && *str)
+    {
+        ft_putchar(*str, ret);
+        str++;
+    }
+}
+
+void    ft_putnbr(int num, int *ret)
+{
+    long    nb;
+
+    nb = num;
+    if (nb == INT_MIN)
+    {
+        write(1, "-2147483648", 12);
+        return ;
+    }
+    if (nb < 0)
+    {
+        ft_putchar('-', ret);
+        ft_putnbr((nb * -1), ret);
+    }
+    if (nb > 9)
+    {
+        ft_putnbr(nb / 10, ret);
+        ft_putnbr(nb % 10, ret);
+    }
+    if (nb < 10)
+        ft_putchar(nb + '0', ret);
+}
+
+void    ft_puthex(unsigned int num, int *ret)
+{
+    char *hex = "0123456789abcdef";
+
+    if (num > 15)
+    {
+        ft_puthex(num / 16, ret);
+        ft_puthex(num % 16, ret);
+    }
+    if (num < 16)
+        ft_putchar(hex[num], ret);
+}
+
+void    specifier(char c, int *ret, va_list *args)
+{
+    if (c == 's')
+    {
+        ft_putstr(va_arg(*args, char *), ret);   
+    }
+    if (c == 'd')
+    {
+        ft_putnbr(va_arg(*args, int), ret);   
+    }
+    if (c == 'x')
+    {
+        ft_puthex(va_arg(*args, unsigned int), ret);   
+    }
+}
+
+int ft_printf(const char *str, ... )
+{
+    int     ret = 0;
+    va_list  args;
+    int     i = 0;
     
+    va_start(args, str);
+    while (str[i])
+    {
+        if (str[i] == '%' && str[i + 1] != '\0')
+        {
+            i++;
+            specifier(str[i], &ret, &args);
+            i++;
+        }
+        else
+        {
+            ft_putchar(str[i], &ret);
+            i++;
+        }
+    }
+    va_end(args);
+    return (ret);
+}
+
+int main(void)
+{
+    ft_printf("Hola, esto funciona\n%s", "si funciona\n");
+    int i = ft_printf("Hola\n%x\n", 42);
+    ft_printf("%d\n", i);
+    return (0);
 }
