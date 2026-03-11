adee21721eafa4211569b8e34c8196eaa4a7129d
Riccardo Spinelli Ranero <rspinell@c1r12s7.42lausanne.ch>
2026-03-11 13:00:45 +0100

    n_queens

--- Exam_Training/Exam03/Level2/n_queens.c
+++ Exam_Training/Exam03/Level2/n_queens.c
@@ -1,4 +1,4 @@
-/* 
+/*
 *Assignement name : n_queens
 
 Expected files : *.c *.h
@@ -26,18 +26,20 @@ $> ./n_queens 4 | cat -e
 $> ./n_queens 7 | cat -e
 0 2 4 6 1 3 5$
 0 3 6 2 5 1 4$
-etc... 
+etc...
 */
 
 #include <stdlib.h>
 #include <unistd.h>
 
-/* 
-- 1. N-QUEENS
+/*
+- N-QUEENS
 * Goal: Place N queens on a board so none can attack each other.
 * Function: Tries placing a queen column by column. For each column, it checks
-* every row. If a spot is safe (no shared row or diagonal), it places the queen 
+* every row. If a spot is safe (no shared row or diagonal), it places the queen
 * and recursively moves to the next column. When it reaches the end, it prints.
+
++ OCEANO TO THE RESCUE: https://www.youtube.com/watch?v=CcufXAmNU2I
 */
 
 void	ft_putchar(char c)
@@ -52,6 +54,8 @@ void	ft_putnbr(int n)
 	ft_putchar((n % 10) + '0');
 }
 
+/*
+* Returns an absolute value of a number.*/
 int	ft_abs(int n)
 {
 	if (n < 0)
@@ -59,6 +63,20 @@ int	ft_abs(int n)
 	return (n);
 }
 
+/*
+* This function takes as params:
++ board: sorting the data of all the positions of the previously placed queens
++ current_col: The column we are trying to place the queen right now
++ row: The specific row we are testing.
+
+- Iterates over the board until arriving to the current_col (no need to check after, those are empty)
+- Then we check:
+	* board[i] = is it at the exact same spot?
+	* ft_abs(board[i] - row) == (current_col - i) = check diagonals
+		* ft_abs(board[i] - row) calculates the vertical distance.
+		* current_col - i = calculates the horizontal distance
+		* If both are equal, it means they are in diagonal.
+- Then we increment to check the next queen.*/
 int	is_safe(int *board, int current_col, int row)
 {
 	int	i;
@@ -73,6 +91,15 @@ int	is_safe(int *board, int current_col, int row)
 	return (1);
 }
 
+/*
+* BASE CASE:
++ if current_col == n, the algo succesfully placed all the queens, so you just print.
+
+* RECURSIVE CASE:
++ This case tries to place a queen in every possible row (from 0 till n - 1)
++ if (is_safe) it records the placement in the board[current_col]
++ calls itself again to tackle the next column (current_col + 1)
+*/
 void	solve(int *board, int current_col, int n)
 {
 	int	row;
